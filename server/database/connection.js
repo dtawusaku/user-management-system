57723cd2f3a42980a3b46ce0bede28dc44cc62c7
dtawusaku <davidtonyawusaku@gmail.com>
2023-04-29 01:58:27 +0100

    Solve

--- server/database/connection.js
+++ server/database/connection.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 const connectDB =  async()=>{
     try{
         //MongoDb Connection String
-<<<<<<< HEAD
-        const con = await mongoose.connect("mongodb+srv://dtawusaku:sHd6TUOF75orAok3@ums.wodsjpd.mongodb.net/?retryWrites=true&w=majority",{
-=======
         const con = await mongoose.connect(process.env.MONGO_URI,{
->>>>>>> a2f42901e71bf200cf639c631e4db3223d0738d0
             useNewUrlParser:true,
             useUnifiedTopology:true,
         });
--- server/routes/router.js
+++ server/routes/router.js
@@ -3,17 +3,9 @@ const route = express.Router();
 const services = require("../services/render")
 
 
-<<<<<<< HEAD
 const services = require("../services/render")
 const controller = require("../controller/controller");
 
-=======
-route.get('/',services.homeRoutes)
-route.get('/add-user',services.add_user)
-route.get('/edit-user',services.edit_user)
-route.get('/update-user',services.update_user)
-route.get('/delete-user',services.delete_user)
->>>>>>> a2f42901e71bf200cf639c631e4db3223d0738d0
 
 route.get('/',services.homeRoutes)
 route.get('/add-user',services.add_user)
